8eb0487a0aabe083782c1ec4adf7308024c6f2f3
Artur Twardy <artur.twardy@mobica.com>
2017-09-13 14:22:47 +0200

    Added Gradient method for detecting eye center

--- QmlComponents/CameraItem.cpp
+++ QmlComponents/CameraItem.cpp
@@ -2,6 +2,7 @@
 
 #include <assert.h>
 #include <thread>
+#include <queue>
 
 #include <QSGGeometryNode>
 #include <QSGGeometry>
@@ -13,20 +14,38 @@
 
 #include "tbb/pipeline.h"
 
+const double kGradientThreshold = 50.0;
+const int kWeightBlurSize = 5;
+const bool kEnablePostProcess = true;
+const float kPostProcessThreshold = 0.97;
+const bool kEnableWeight = true;
+const bool kPlotVectorField = false;
+const float kWeightDivisor = 1.0;
+const int kFastEyeWidth = 50;
+const int kEyePercentTop = 25;
+const int kEyePercentSide = 13;
+const int kEyePercentHeight = 25;
+const int kEyePercentWidth = 35;
+const bool kSmoothFaceImage = false;
+const float kSmoothFaceFactor = 0.005;
+
+using namespace cv;
+
 CameraItem::CameraItem()
     : frameRate(this, &CameraItem::frameRateChanged, 15)
-    , width(this, &CameraItem::widthChanged, 640)
-    , height(this, &CameraItem::heightChanged, 480)
+    , videoWidth(this, &CameraItem::videoWidthChanged, 640)
+    , videoHeight(this, &CameraItem::videoHeightChanged, 480)
     , cameraInterface(this, &CameraItem::cameraInterfaceChanged, 0)
     , firstCascadeSource(this, &CameraItem::firstCascadeSourceChanged, ":/cascades/haarcascade_frontalface_alt.xml")
     , secondCascadeSource(this, &CameraItem::secondCascadeSourceChanged, ":/cascades/haarcascade_eye.xml")
     , m_Done(false)
+    , m_FacePos()
 {
-    cv::ocl::setUseOpenCL(true);
+    ocl::setUseOpenCL(true);
 
     m_Capture.set(CV_CAP_PROP_FPS, frameRate);
-    m_Capture.set(CV_CAP_PROP_FRAME_WIDTH, width);
-    m_Capture.set(CV_CAP_PROP_FRAME_HEIGHT, height);
+    m_Capture.set(CV_CAP_PROP_FRAME_WIDTH, videoWidth);
+    m_Capture.set(CV_CAP_PROP_FRAME_HEIGHT, videoHeight);
 
     bool connected = connect(this, &CameraItem::capturedImage, this, &CameraItem::setImage, Qt::QueuedConnection);
     assert(connected);
@@ -82,7 +101,7 @@ QSGNode* CameraItem::updatePaintNode(QSGNode *oldNode, UpdatePaintNodeData *)
 
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, m_Image.cols, m_Image.rows, 0, GL_BGR, GL_UNSIGNED_BYTE, m_Image.ptr());
 
-    QSGTexture *t = window()->createTextureFromId(static_cast<unsigned int>(*texture), QSize(width, height));
+    QSGTexture *t = window()->createTextureFromId(static_cast<unsigned int>(*texture), QSize(videoWidth, videoHeight));
 
     delete[] texture;
 
@@ -150,23 +169,23 @@ bool CameraItem::_loadCascade(CameraItem::Cascade &cascade, QString url)
     return true;
 }
 
-void CameraItem::_detectAndDrawTBB(cv::VideoCapture &capture,
+void CameraItem::_detectAndDrawTBB(VideoCapture &capture,
                                   CameraItem::Concurent_queue &guiQueue,
                                   CameraItem::Cascade &cascade,
                                   CameraItem::Cascade &nestedCascade,
                                   double scale,
                                   bool tryFlip)
 {
-    const static cv::Scalar colors[] =
+    const static Scalar colors[] =
         {
-            cv::Scalar(255,0,0),
-            cv::Scalar(255,128,0),
-            cv::Scalar(255,255,0),
-            cv::Scalar(0,255,0),
-            cv::Scalar(0,128,255),
-            cv::Scalar(0,255,255),
-            cv::Scalar(0,0,255),
-            cv::Scalar(255,0,255)
+            Scalar(255,0,0),
+            Scalar(255,128,0),
+            Scalar(255,255,0),
+            Scalar(0,255,0),
+            Scalar(0,128,255),
+            Scalar(0,255,255),
+            Scalar(0,0,255),
+            Scalar(255,0,255)
         };
 
     tbb::parallel_pipeline(7,
@@ -190,7 +209,7 @@ void CameraItem::_detectAndDrawTBB(cv::VideoCapture &capture,
     tbb::make_filter<ProcessingChainData*, ProcessingChainData *>(tbb::filter::serial_in_order,
                                            [&](ProcessingChainData *pData)->ProcessingChainData*
     {
-        cv::cvtColor(pData->image, pData->gray, cv::COLOR_BGR2GRAY);
+        cvtColor(pData->image, pData->gray, COLOR_BGR2GRAY);
         return pData;
     }
     )&
@@ -198,14 +217,14 @@ void CameraItem::_detectAndDrawTBB(cv::VideoCapture &capture,
                                            [&](ProcessingChainData* pData)->ProcessingChainData*
     {
         double fx = 1 / scale;
-        cv::resize(pData->gray, pData->smallImg, cv::Size(), fx, fx, cv::INTER_LINEAR);
+        resize(pData->gray, pData->smallImg, Size(), fx, fx, INTER_LINEAR);
         return pData;
     }
     )&
     tbb::make_filter<ProcessingChainData*, ProcessingChainData*>(tbb::filter::serial_in_order,
                                            [&](ProcessingChainData* pData)->ProcessingChainData*
     {
-        cv::equalizeHist(pData->smallImg, pData->smallImg);
+        equalizeHist(pData->smallImg, pData->smallImg);
         return pData;
     }
     )&
@@ -213,12 +232,12 @@ void CameraItem::_detectAndDrawTBB(cv::VideoCapture &capture,
                                            [&](ProcessingChainData* pData)->ProcessingChainData*
     {
         cascade.detectMultiScale(pData->smallImg, pData->firstCascadeObjects,
-                                 1.1, 2, 0 | cv::CASCADE_SCALE_IMAGE,
-                                 cv::Size(30, 30));
+                                 1.05, 3, 0 | CASCADE_SCALE_IMAGE,
+                                 Size(150, 150));
 
 //        if (tryFlip)
 //        {
-//            cv::flip(pData->image, pData->image, 1);
+//            flip(pData->image, pData->image, 1);
 //        }
 
         return pData;
@@ -231,44 +250,32 @@ void CameraItem::_detectAndDrawTBB(cv::VideoCapture &capture,
         {
             for (size_t i = 0; i < pData->firstCascadeObjects.size(); ++i)
             {
-                cv::Rect& item = pData->firstCascadeObjects[i];
+                m_FacePos = pData->firstCascadeObjects[i];
 
-                cv::Point center;
-                cv::Scalar color = colors[i%8];
+                Rect smoothedRect = _getSmoothed(m_FacePos);
+                Scalar color = colors[i%8];
+                Rect faceRect = {cvPoint(cvRound(smoothedRect.x * scale), cvRound(smoothedRect.y * scale)),
+                                     cvPoint(cvRound((smoothedRect.x + smoothedRect.width) * scale),
+                                             cvRound((smoothedRect.y + smoothedRect.height) * scale))};
 
-                cv::rectangle( pData->image, cvPoint(cvRound(item.x*scale), cvRound(item.y*scale)),
-                               cvPoint(cvRound((item.x + item.width-1)*scale), cvRound((item.y + item.height-1)*scale)),
-                               color, 3, 8, 0);
+                rectangle(pData->image, faceRect, color, 3, 8, 0);
 
-                const int half_height=cvRound(static_cast<float>(item.height/2));
-                const int half_width = cvRound(static_cast<float>(item.width / 2));
-                item.y=item.y + half_height / 2;
-                item.height = half_height-1;
-                item.x= item.x + item.width / 2;
-                cv::Mat smallImgROI = pData->smallImg(item);
+                // draw eyes
+                int eye_region_width = faceRect.width * (kEyePercentWidth/100.0);
+                int eye_region_height = faceRect.width * (kEyePercentHeight/100.0);
+                int eye_region_top = faceRect.height * (kEyePercentTop/100.0);
+                Rect leftEyeRegion(smoothedRect.x + faceRect.width * (kEyePercentSide/100.0),
+                                       smoothedRect.y + eye_region_top, eye_region_width, eye_region_height);
+                Rect rightEyeRegion(smoothedRect.x + faceRect.width - eye_region_width - faceRect.width * (kEyePercentSide/100.0),
+                                        smoothedRect.y + eye_region_top, eye_region_width, eye_region_height);
 
-                // detect eyes
-                nestedCascade.detectMultiScale( smallImgROI, pData->secondCascadeObjects,
-                                                1.1, 3, 0|cv::CASCADE_SCALE_IMAGE,
-                                                cv::Size(30, 30) );
+                rectangle(pData->image, leftEyeRegion, color, 3, 8, 0);
+                rectangle(pData->image, rightEyeRegion, color, 3, 8, 0);
 
-                if (!pData->secondCascadeObjects.empty())
-                {
-                    for (size_t j = 0; j < pData->secondCascadeObjects.size(); ++j)
-                    {
-                        cv::Rect& eye = pData->secondCascadeObjects.at(j);
-                        center.x = cvRound((eye.x + eye.width / 2 + item.x) * scale);
-                        center.y = cvRound((eye.y + eye.height / 2 + item.y) * scale);
-                        int radius = cvRound((eye.width + eye.height) * 0.25 * scale);
-
-                        cv::Rect eye_rect = cv::Rect(item.x + eye.x, eye.y + item.y, eye.width, eye.height);
-                        cv::Mat eye_img = pData->smallImg(eye_rect);
-
-                        // todo: hough circles
-
-                        cv::rectangle(pData->image, eye_rect, color);
-                    }
-                }
+                Mat faceROI = pData->image(faceRect);
+                Point leftPupil = _getSmoothed(_findEyeCenter(pData->image, leftEyeRegion));
+//                qDebug()<<"leftPupil: "<<leftPupil.x<<", "<<leftPupil.y;
+                circle(pData->image, cvPoint(leftPupil.x + faceRect.x, leftPupil.y + faceRect.y + leftEyeRegion.height / 2), 3, 1234);
             }
         }
         return pData;
@@ -293,6 +300,257 @@ void CameraItem::_detectAndDrawTBB(cv::VideoCapture &capture,
     }
     )
     );
+}
+
+Rect CameraItem::_getSmoothed(const Rect& point)
+{
+    static constexpr int listSize = 5;
+    static Rect list[listSize] = {Rect()};
+    static short pos = 0;
+
+    if (pos == listSize) pos = 0;
+    list[pos] = point;
+    ++pos;
+
+    int x = 0;
+    int y = 0;
+    int w = 0;
+    int h = 0;
+    for (int i = 0; i < listSize; ++i)
+    {
+        x += list[i].x;
+        y += list[i].y;
+        w += list[i].width;
+        h += list[i].height;
+    }
+
+    return Rect(x / listSize, y / listSize, w / listSize, h / listSize);
+}
+
+Point CameraItem::_getSmoothed(const Point& point)
+{
+    static constexpr int listSize = 10;
+    static Point list[listSize] = {Point()};
+    static short pos = 0;
+
+    if (pos == listSize) pos = 0;
+    list[pos] = point;
+    ++pos;
+
+    int x = 0;
+    int y = 0;
+    for (int i = 0; i < listSize; ++i)
+    {
+        x += list[i].x;
+        y += list[i].y;
+    }
+
+    return Point(x / listSize, y / listSize);
+}
+
+Point CameraItem::_findEyeCenter(Mat face, Rect eye)
+{
+    Mat eyeROIUnscaled = face(eye);
+    Mat eyeROI;
+
+    _scaleToFastSize(eyeROIUnscaled, eyeROI);
+
+    Mat gradientX = _computeMatXGradient(eyeROI);
+    Mat gradientY = _computeMatXGradient(eyeROI.t()).t();
+
+    Mat mags = _getMatrixMagnitude(gradientX, gradientY);
+
+    // compute dynamic threshold
+    Scalar stdMagnGrad, meanMagnGrad;
+    meanStdDev(mags, meanMagnGrad, stdMagnGrad);
+    // ?? square root?
+    double stdDev = stdMagnGrad[0] / std::sqrt(mags.rows*mags.cols);
+    double dynamicThreshold = kGradientThreshold * stdDev + meanMagnGrad[0];
+
+    // normalize
+    for (int y = 0; y <eyeROI.rows; ++y) {
+        double *Xr = gradientX.ptr<double>(y), *Yr = gradientY.ptr<double>(y);
+        const double *Mr = mags.ptr<double>(y);
+        for (int x = 0; x <eyeROI.cols; ++x) {
+            double gX = Xr[x], gY = Yr[x];
+            double magnitude = Mr[x];
+            if (magnitude > dynamicThreshold) {
+                Xr[x] = gX/magnitude;
+                Yr[x] = gY/magnitude;
+            } else {
+                Xr[x] = 0.0;
+                Yr[x] = 0.0;
+            }
+        }
+    }
+
+    //-- Create a blurred and inverted image for weighting
+    Mat weight;
+    GaussianBlur(eyeROI, weight, Size( kWeightBlurSize, kWeightBlurSize ), 0, 0 );
+    for (int y = 0; y < weight.rows; ++y) {
+        unsigned char *row = weight.ptr<unsigned char>(y);
+        for (int x = 0; x < weight.cols; ++x) {
+            row[x] = (255 - row[x]);
+        }
+    }
+
+    Mat outSum = Mat::zeros(eyeROI.rows,eyeROI.cols,CV_64F);
+
+    for (int y = 0; y < weight.rows; ++y) {
+        const double *Xr = gradientX.ptr<double>(y), *Yr = gradientY.ptr<double>(y);
+        for (int x = 0; x < weight.cols; ++x) {
+            double gX = Xr[x], gY = Yr[x];
+            if (gX == 0.0 && gY == 0.0) {
+                continue;
+            }
+            _testPossibleCentersFormula(x, y, weight, gX, gY, outSum);
+        }
+    }
+
+    // scale all the values down, basically averaging them
+    double numGradients = (weight.rows*weight.cols);
+    Mat out;
+    outSum.convertTo(out, CV_32F,1.0/numGradients);
+    //imshow(debugWindow,out);
+    //-- Find the maximum point
+    Point maxP;
+    double maxVal;
+    minMaxLoc(out, NULL,&maxVal,NULL,&maxP);
+    //-- Flood fill the edges
+    if(kEnablePostProcess) {
+        Mat floodClone;
+        //double floodThresh = computeDynamicThreshold(out, 1.5);
+        double floodThresh = maxVal * kPostProcessThreshold;
+        threshold(out, floodClone, floodThresh, 0.0f, THRESH_TOZERO);
+        if(kPlotVectorField) {
+            //plotVecField(gradientX, gradientY, floodClone);
+            //                   imwrite("eyeFrame.png",eyeROIUnscaled);
+        }
+        Mat mask = _floodKillEdges(floodClone);
+        // redo max
+        minMaxLoc(out, NULL,&maxVal,NULL,&maxP,mask);
+    }
+    return _unscalePoint(maxP, eye);
+}
+
+Mat CameraItem::_floodKillEdges(Mat &mat) {
+    rectangle(mat,Rect(0,0,mat.cols,mat.rows),255);
+
+    Mat mask(mat.rows, mat.cols, CV_8U, 255);
+    std::queue<Point> toDo;
+    toDo.push(Point(0,0));
+    while (!toDo.empty()) {
+        Point p = toDo.front();
+        toDo.pop();
+        if (mat.at<float>(p) == 0.0f) {
+            continue;
+        }
+        // add in every direction
+        Point np(p.x + 1, p.y); // right
+        if (_floodShouldPushPoint(np, mat)) toDo.push(np);
+        np.x = p.x - 1; np.y = p.y; // left
+        if (_floodShouldPushPoint(np, mat)) toDo.push(np);
+        np.x = p.x; np.y = p.y + 1; // down
+        if (_floodShouldPushPoint(np, mat)) toDo.push(np);
+        np.x = p.x; np.y = p.y - 1; // up
+        if (_floodShouldPushPoint(np, mat)) toDo.push(np);
+        // kill it
+        mat.at<float>(p) = 0.0f;
+        mask.at<uchar>(p) = 0;
+    }
+    return mask;
+}
+
+Point CameraItem::_unscalePoint(Point p, Rect origSize)
+{
+    float ratio = (((float)kFastEyeWidth)/origSize.width);
+    int x = round(p.x / ratio);
+    int y = round(p.y / ratio);
+    return Point(x,y);
+}
+
+bool CameraItem::_floodShouldPushPoint(const Point &np, const Mat &mat)
+{
+    return _inMat(np, mat.rows, mat.cols);
+}
+
+bool CameraItem::_inMat(Point p,int rows,int cols)
+{
+    return p.x >= 0 && p.x < cols && p.y >= 0 && p.y < rows;
+}
+
+void CameraItem::_testPossibleCentersFormula(int x, int y, const Mat &weight,double gx, double gy, Mat &out) {
+    // for all possible centers
+    for (int cy = 0; cy < out.rows; ++cy) {
+        double *Or = out.ptr<double>(cy);
+        const unsigned char *Wr = weight.ptr<unsigned char>(cy);
+        for (int cx = 0; cx < out.cols; ++cx) {
+            if (x == cx && y == cy) {
+                continue;
+            }
+            // create a vector from the possible center to the gradient origin
+            double dx = x - cx;
+            double dy = y - cy;
+            // normalize d
+            double magnitude = sqrt((dx * dx) + (dy * dy));
+            dx = dx / magnitude;
+            dy = dy / magnitude;
+            double dotProduct = dx*gx + dy*gy;
+            dotProduct = std::max(0.0,dotProduct);
+            // square and multiply by the weight
+            if (kEnableWeight) {
+                Or[cx] += dotProduct * dotProduct * (Wr[cx]/kWeightDivisor);
+            } else {
+                Or[cx] += dotProduct * dotProduct;
+            }
+        }
+    }
+}
+
+Mat CameraItem::_computeMatXGradient(const Mat& mat)
+{
+    Mat out(mat.rows, mat.cols, CV_64F);
+
+    for (int y = 0; y < mat.rows; ++y)
+    {
+        const uchar *Mr = mat.ptr<uchar>(y);
+        double *Or = out.ptr<double>(y);
+
+        Or[0] = Mr[1] - Mr[0];
+        for (int x = 1; x < mat.cols - 1; ++x)
+        {
+            Or[x] = (Mr[x+1] - Mr[x-1])/2.0;
+        }
+
+        Or[mat.cols-1] = Mr[mat.cols-1] - Mr[mat.cols-2];
+    }
+
+    return out;
+}
+
+Mat CameraItem::_getMatrixMagnitude(const Mat &matX, const Mat &matY)
+{
+    Mat mags(matX.rows, matX.cols, CV_64F);
+
+    for (int y = 0; y < matX.rows; ++y)
+    {
+        const double *Xr = matX.ptr<double>(y);
+        const double *Yr = matY.ptr<double>(y);
+        double *Mr = mags.ptr<double>(y);
+
+        for (int x = 0; x < matX.cols; ++x)
+        {
+            double gX = Xr[x], gY = Yr[x];
+            double magnitude = sqrt((gX * gX) + (gY * gY));
+            Mr[x] = magnitude;
+        }
+    }
+    return mags;
+}
+
+void CameraItem::_scaleToFastSize(const Mat &src,Mat &dst)
+{
+resize(src, dst, Size(kFastEyeWidth,(((float)kFastEyeWidth)/src.cols) * src.rows));
 }
 
 void CameraItem::setImage()
--- QmlComponents/CameraItem.h
+++ QmlComponents/CameraItem.h
@@ -18,8 +18,8 @@ class CameraItem : public QQuickItem
 {
     Q_OBJECT
     Q_PROPERTY(int frameRate READ frameRate WRITE frameRate NOTIFY frameRateChanged)
-    Q_PROPERTY(int width READ width WRITE width NOTIFY widthChanged)
-    Q_PROPERTY(int height READ height WRITE height NOTIFY heightChanged)
+    Q_PROPERTY(int videoWidth READ videoWidth WRITE videoWidth NOTIFY videoWidthChanged)
+    Q_PROPERTY(int videoHeight READ videoHeight WRITE videoHeight NOTIFY videoHeightChanged)
     Q_PROPERTY(int cameraInterface READ cameraInterface WRITE cameraInterface NOTIFY cameraInterfaceChanged)
     Q_PROPERTY(QString firstCascadeSource READ firstCascadeSource WRITE firstCascadeSource NOTIFY firstCascadeSourceChanged)
     Q_PROPERTY(QString secondCascadeSource READ secondCascadeSource WRITE secondCascadeSource NOTIFY secondCascadeSourceChanged)
@@ -37,16 +37,16 @@ public:
     ~CameraItem();
 
     QPropertyWrapper<int> frameRate;
-    QPropertyWrapper<int> width;
-    QPropertyWrapper<int> height;
+    QPropertyWrapper<int> videoWidth;
+    QPropertyWrapper<int> videoHeight;
     QPropertyWrapper<int> cameraInterface;
     QPropertyWrapper<QString> firstCascadeSource;
     QPropertyWrapper<QString> secondCascadeSource;
 
 signals:
     void frameRateChanged();
-    void widthChanged();
-    void heightChanged();
+    void videoWidthChanged();
+    void videoHeightChanged();
     void cameraInterfaceChanged();
     void firstCascadeSourceChanged();
     void secondCascadeSourceChanged();
@@ -65,6 +65,19 @@ private:
                           Cascade& nestedCascade,
                           double scale, bool tryFlip);
     void setImage();
+    cv::Rect _getSmoothed(const cv::Rect &point);
+
+    // gradient algorithms
+    cv::Point _findEyeCenter(cv::Mat face, cv::Rect eye);
+    cv::Mat _floodKillEdges(cv::Mat &mat);
+    cv::Point _unscalePoint(cv::Point p, cv::Rect origSize);
+    bool _floodShouldPushPoint(const cv::Point &np, const cv::Mat &mat);
+    bool _inMat(cv::Point p, int rows, int cols);
+    void _testPossibleCentersFormula(int x, int y, const cv::Mat &weight, double gx, double gy, cv::Mat &out);
+    cv::Mat _computeMatXGradient(const cv::Mat &mat);
+    cv::Mat _getMatrixMagnitude(const cv::Mat &matX, const cv::Mat &matY);
+    void _scaleToFastSize(const cv::Mat &src, cv::Mat &dst);
+    cv::Point _getSmoothed(const cv::Point &point);
 
     cv::VideoCapture m_Capture;
     std::atomic<bool> m_Done;
@@ -75,7 +88,7 @@ private:
     cv::Mat m_Image;
     std::thread m_PipelineRunner;
     Concurent_queue m_GuiQueue;
-    
+    cv::Rect m_FacePos;
 };
 
 #endif // CAMERAITEM_H
